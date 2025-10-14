4a659cb095ceddcdb51f89f739ca70791b78e588
DOUG-NOTE\ssdou <ssdoug10@gmail.com>
2025-10-14 20:40:07 -0300

    ajsute Fast API

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ def get_brasilia_time():
     brasilia_tz = pytz.timezone("America/Sao_Paulo")
     now = datetime.now(brasilia_tz)
     data_hora = now.strftime("%Y-%m-%d %H:%M:%S")
-    return {"Bem-vindo à API Hora de Brasília!\\n Data_hora_brasilia": data_hora}
+    return {"Bem-vindo à API Hora de Brasília!\n Data_hora_brasilia": data_hora}
