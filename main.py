0064c00cf5231434659e46ff84a2da57cc840dc1
DOUG-NOTE\ssdou <ssdoug10@gmail.com>
2025-10-14 20:41:13 -0300

    up

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from datetime import datetime
+from fastapi.middleware.cors import CORSMiddleware
 import pytz
 
 app = FastAPI(title="API Hora de Brasília")
+
+# Permitir acesso de qualquer origem (para desktop apps e testes)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
