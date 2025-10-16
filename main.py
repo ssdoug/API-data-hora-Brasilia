d8d12ff9273c60a4fe5b3abe9399e66b63bfa8ab
DOUG-NOTE\ssdou <ssdoug10@gmail.com>
2025-10-15 22:13:03 -0300

    atz nome campo de retorno full

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ def get_brasilia_time():
     data_hora = now.strftime("%Y-%m-%d %H:%M:%S")
     return {"data_hora_brasilia": data_hora}
 
-@app.get("/tot")
+@app.get("/full")
 def get_brasilia_time():
     brasilia_tz = pytz.timezone("America/Sao_Paulo")
     now = datetime.now(brasilia_tz)
     data_hora = now.strftime("%Y-%m-%d %H:%M:%S")
-    return {"Bem-vindo à API Hora de Brasília! Atual: ": data_hora}
+    return {"message": data_hora}
