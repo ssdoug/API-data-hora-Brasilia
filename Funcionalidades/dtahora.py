9a34504f81786abfe22bbeb89a20973de1c26e2f
DOUG-NOTE\ssdou <ssdoug10@gmail.com>
2025-10-15 22:39:38 -0300

    atz full

--- Funcionalidades/dtahora.py
+++ Funcionalidades/dtahora.py
@@ -17,7 +17,7 @@ def get_brasilia_time_full():
     brasilia_tz = pytz.timezone("America/Sao_Paulo")
     now = datetime.now(brasilia_tz)
     data_hora = now.strftime("%Y-%m-%d %H:%M:%S")
-    return {"Bem-vindo à API Hora de Brasília! Atual:": data_hora}
+    return {"data_hora_brasilia": f"Teste {data_hora}"}
 
 def get_test():
-    return {"data_hora_brasilia": "Funcionou o método!"}
+    return {"data_hora_brasilia": "endpoint de teste funcionando!"}
--- main.py
+++ main.py
@@ -16,23 +16,17 @@ app.add_middleware(
 )
 
 @app.get("/")
-def read_root():
-    return {"message": "Bem-vindo à API Hora de Brasília!"}
-
-#@app.get("/hora")
-#def get_brasilia_time():
-#    brasilia_tz = pytz.timezone("America/Sao_Paulo")
-#    now = datetime.now(brasilia_tz)
-#    data_hora = now.strftime("%Y-%m-%d %H:%M:%S")
-#    return {"data_hora_brasilia": data_hora}
-
-@app.get("/full")
-def get_brasilia_time():
-    brasilia_tz = pytz.timezone("America/Sao_Paulo")
-    now = datetime.now(brasilia_tz)
-    data_hora = now.strftime("%Y-%m-%d %H:%M:%S")
-    return {"message": data_hora}
+def read_root_():
+    return dtahora.read_root()
 
 @app.get("/hora")
+def get_hora():
+    return dtahora.get_brasilia_time()
+
+@app.get("/full")
+def read_full():
+    return dtahora.get_brasilia_time_full()
+
+@app.get("/test")
 def get_teste():
     return dtahora.get_test()
